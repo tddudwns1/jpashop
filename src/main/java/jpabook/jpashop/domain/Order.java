f01d88c6ab61eef16e90b28ba314dedbc8bae223
seong yeong jun <tjddudwns1@naver.com>
2023-12-31 00:13:46 +0900

    [feat] 실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ID")
+    private Long id;
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
